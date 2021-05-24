108ba3c6ec01dde25e4eca3bae34ebfc5fb24604
Luis Prado <luisprado0095@gmail.com>
2021-05-23 23:26:53 -0300

    :memo:

--- 45-Clases/src/palabrathis/PalabraThis.java
+++ 45-Clases/src/palabrathis/PalabraThis.java
@@ -11,13 +11,33 @@ package palabrathis;
  */
 public class PalabraThis {
     public static void main(String[] args) {
+        Persona personaUno = new Persona("Luis","Prado");
+        System.out.println("Nombre = " + personaUno.nombre);
+        System.out.println("Apellido = " + personaUno.apellido);
+        
         
     }
 }
-
+// si creamos un clase dentro de otra no se agrega el acceso public
+//estas clases van a estar definidas dentro de este paquete
 class Persona{
     
     String nombre;
-    String Apelliido;
+    String apellido;
     
+    Persona(String nombre,String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        System.out.println("Objeto persona this:"+ this);
+        //Imprimir imprimir = new Imprimir() 
+        new Imprimir().imprimir(this);
+    }
+}
+
+class Imprimir{
+    
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde imprimir: " + persona);
+        System.out.println("impresion de objeto this" + this);
+    }
 }
---
+++ Encapsulamiento/src/test/PersonaPrueba.java
@@ -0,0 +1,16 @@
+
+package test;
+import dominio.Persona;
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        
+        Persona pUno = new Persona("Luis",50000.00,false);
+        
+        System.out.println("Nombre: "+ pUno.getNombre());
+        System.out.println("Sueldo: "+ pUno.getSueldo());
+        System.out.println("Eliminado: "+ pUno.isEliminado());
+        
+        
+    }
+}
